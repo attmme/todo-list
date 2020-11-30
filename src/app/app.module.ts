4d9592863d92ba33150a9ddab50c9095456a678b
Oscar <ogomezsts51l@gmail.com>
2020-11-30 12:17:51 +0100

    save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { ItemComponent } from './components/item/item.component';
 import { ErrorComponent } from './components/error/error.component';
-import { MessageComponent } from './components/error/message/message.component';
 import { ListComponent } from './components/display/list/list.component';
 import { CardComponent } from './components/display/card/card.component';
+import { MessageComponent } from './components/error/message/message.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent,
     HeaderComponent,
+    FooterComponent,
     ItemComponent,
     ErrorComponent,
-    MessageComponent,
     ListComponent,
-    CardComponent
+    CardComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
