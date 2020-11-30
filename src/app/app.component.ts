47e3623e56da7b1ffc4518471be8b7f9d3222046
Oscar <ogomezsts51l@gmail.com>
2020-11-30 12:24:45 +0100

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+  showFooter = true;
+
+  toogleFooter() {
+    this.showFooter = !this.showFooter;
+  }
 }
+
